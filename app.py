7a8a54da81a1cb45bd834579bd6c69cb12e40444
nishiko0124 <19s4023m@gmail.com>
2025-12-17 17:28:12 +0900

    フラッシュ

--- app.py
+++ app.py
@@ -436,4 +436,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
